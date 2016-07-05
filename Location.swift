cb55d63388e0b72c33196ed15d530faa6ad280c8
yufeiyang1995 <nju@njus-iMac.local>
2016-07-05 13:42:05 +0800

    debug

---
+++ Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  demoGame
+//
+//  Created by nju on 16/5/22.
+//  Copyright © 2016年 nju. All rights reserved.
+//
+
+import Foundation
+
+class Location{
+    var x = 0
+    var y = 0
+}
